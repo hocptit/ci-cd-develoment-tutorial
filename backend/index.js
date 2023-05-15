bb4f73181bd2aacf10343844d193cb5f5c618c1b
Patrick Nguyen <nguyenthaihoc.dev@gmail.com>
2023-05-16 00:27:49 +0700

    feat: add day 1

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+// const mongoUri = process.env.mongoUri || 'mongodb://admin:admin@localhost:27017/admin'
+// mongoose.connect(mongoUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('MongoDB connected!');
+// }).catch((err) => {
+//   console.error('Failed to connect MongoDB:', err);
+// });
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  createdDate: { type: Date, default: Date.now },
+});
+
+
+
+app.get('/api/article', async (req, res) => {
+  const Article = mongoose.model('Article', articleSchema);
+  const article = new Article({
+    title: 'Docker cơ bản 1',
+    content: 'You will likely find yourself rebuilding the same Docker image over and over again. Whether it’s for the next release of your software, or locally during development. Because building images is a common task, Docker provides several tools that speed up builds.'
+  });
+  // await article.save()
+  console.log(article)
+  res.json(article)
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
